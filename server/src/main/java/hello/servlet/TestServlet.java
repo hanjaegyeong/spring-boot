6024aecfb8597e369b56f0d4651ed802ec67cd9f
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2024-02-05 19:21:21 +0900

    feat: 프로젝트 설정

---
+++ server/src/main/java/hello/servlet/TestServlet.java
@@ -0,0 +1,18 @@
+package hello.servlet;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * http://localhost:8080/test
+ */
+@WebServlet(urlPatterns = "/test")
+public class TestServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        System.out.println("TestServlet.service");
+        resp.getWriter().println("test");
+    }
+}
