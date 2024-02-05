22900a712ea286c8304dda6b9b4adbd5153ff721
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2024-02-05 19:49:58 +0900

    feat: 서블릿 컨테이너 초기화2

---
+++ server/src/main/java/hello/container/AppInitV1Servlet.java
@@ -0,0 +1,19 @@
+package hello.container;
+
+import hello.servlet.HelloServlet;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletRegistration;
+
+/**
+ * http://localhost:8080/hello-servlet
+ */
+public class AppInitV1Servlet implements AppInit {
+    @Override
+    public void onStartup(ServletContext servletContext) {
+        System.out.println("AppInitV1Servlet.onStartup");
+        //순수 서블릿 코드 등록
+        ServletRegistration.Dynamic helloServlet
+                = servletContext.addServlet("helloServlet", new HelloServlet());
+        helloServlet.addMapping("/hello-servlet");
+    }
+}
